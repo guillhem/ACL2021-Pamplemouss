240aba0acece2adad41e550089f6424009423ea5
cath-fmarie <sabrinaallouch@hotmail.com>
2022-01-09 04:18:41 +0100

    ragequit

---
+++ TP.py
@@ -0,0 +1,8 @@
+"classe des cases de téléportation"
+
+class TP():
+    def __init__(self, destination=[1,1]):
+        self.__dest = destination
+
+    def getDestination(self):
+        return(self.__dest)
