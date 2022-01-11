749a4e5155ab7b9d4eb6ff79f28d5751e208c86f
Goodsaho <92717745+Goodsaho@users.noreply.github.com>
2022-01-11 02:11:32 +0100

    Add files via upload

--- Personnage2.py
+++ Personnage2.py
@@ -5,87 +5,147 @@
 #%% importations 
 
 from Lab import *
-import pygame
 from param import *
+from Monstre2 import *
+from TP import *
 
 
 
 #%%
-nbVies = 1
+
 
 class Personnage():
-    def __init__(self, labDuPerso, nbVies, nbPersonnages=1):
-        self.position=[1,1] # [1,1] case en haut à gauche
-        self.etat=nbVies # nombre de vies
-        self.win=False # = True quand le personnage a atteint la sortie
-        self.labDuPerso=labDuPerso.matrice
+    def __init__(self, labDuPerso, nbVies):
+        self.__labDuPerso=labDuPerso.getMatrice()
+        self.__position=np.where(self.__labDuPerso==3)[0][0], np.where(self.__labDuPerso==3)[1][0]  #position initiale
+        self.__etat=nbVies # nombre de vies
+        self.__win=False # = True quand le personnage a atteint la sortie
         
-    def explicationsDeplacement(self):
-        print("Pour vous déplacer, vous allez utiliser les touches q,s,z,d :\n \
-              q pour vous délacer à gauche,\n \
-              d pour vous délacer à droite,\n \
-              s pour vous délacer en bas,\n \
-              z pour vous délacer en haut.")
+        self.__compteur =1
+        self.__dCompteur=20 #dans la fonction checkCompteur
+        self.__monstres = {}
+       
+        
+        
+    def get_etat(self) :
+        return(self.__etat)
+    def get_win(self) :
+        return(self.__win)
+    def get_lab(self) :
+        return(self.__labDuPerso)
+    def get_monstres(self):
+        return(self.__monstres)
 
+    def initMonstres(self): # pour initialiser le dico monstres
+        n=np.size(self.__labDuPerso[0])    
+        for i in range(n):
+            for j in range(n):
+                if self.__labDuPerso[i,j]==8:
+                    self.__monstres[str(i)+" "+str(j)]= Monstre([i,j], self.__labDuPerso,8)
+                if self.__labDuPerso[i,j]==9:
+                    self.__monstres[str(i)+" "+str(j)]= Monstre([i,j], self.__labDuPerso,9)
+            
+                    #nom du monstre dans le dico "i j" sa position de départ
+                
         
         
+        
+    def set_changement(self,i,j,new): #non utilisé
+        self.__labDuPerso[i][j]=new
+
+
+    def incrementeCompteur(self):
+        self.__compteur+=1
+
+    def diminuter_dCompteur(self):
+        d=self.__dCompteur
+        if d>1:
+            self.__dCompteur=(self.__dCompteur+1)//2
+        # 20 10 5 3 1
+
+    def checkCompteur(self,):
+        
+        if self.__compteur%self.__dCompteur == 0:
+            for monstre in self.__monstres.values():
+                monstre.order()
+        
+    
+
+
+    def direct(self,i,j,posi,posj) :   #posi, posj position actuelle du héros
+        
+        if self.__labDuPerso[i][j]==0: # si la case est vide
+            self.__labDuPerso[posi][posj]=0 # le personnage se déplace dans le lab
+            self.__labDuPerso[i][j]=3
+            self.__position=[i,j] # on change son paramètre position
+            
+        elif self.__labDuPerso[i][j]==2 :
+            self.__win=True
+            
+        elif self.__labDuPerso[i][j]==4 :
+            self.__etat-=1
+        
+        elif self.__labDuPerso[i][j]==6 :  # case PV
+            if self.__etat < PV_max :
+                self.__etat+=1
+            self.__labDuPerso[posi][posj]=0 # le personnage se déplace dans le lab
+            self.__labDuPerso[i][j]=3 # potion utilisée
+            self.__position=[i,j]
+            
+        elif self.__labDuPerso[i][j] ==7: #case accélération
+            self.diminuter_dCompteur()
+            self.__labDuPerso[i][j]=0 # la case acceleration disparait
+            
+        elif self.__labDuPerso[i][j] ==8 or self.__labDuPerso[i][j] ==9 : #les monstres
+            
+            for monstre in self.__monstres.keys() :   # cherche le monstre au niveau du perso
+                mi,mj = monstre.split(' ')
+                if (mi == str(i) or mj == str(j)) and self.__monstres[monstre].getTouchePerso() == False :
+                    self.__etat -= 1
+
+
+
+
     def deplacement(self,direction):
-        
-        i,j = self.position
+
+        i,j = self.__position
         
         if direction=="q": # gauche
-            if self.labDuPerso[i][j-1]==0: # si la case est vide
-                self.labDuPerso[i][j]=0 # le personnage se déplace dans le lab
-                self.labDuPerso[i][j-1]=3
-                self.position=[i,j-1] # on change son paramètre position
-                
-            elif self.labDuPerso[i][j-1]==2:
-                self.win=True
-                
-            elif self.labDuPerso[i][j-1]==4:
-                self.etat-=1
+            self.direct(i,j-1,i,j)
                 
                 
         if direction=="s": # bas
-            if self.labDuPerso[i+1][j]==0:
-                self.labDuPerso[i][j]=0
-                self.labDuPerso[i+1][j]=3
-                self.position=[i+1,j]
+           self.direct(i+1,j,i,j)
                 
-            elif self.labDuPerso[i+1][j]==2:
-                self.win=True
-                
-            elif self.labDuPerso[i+1][j]==4:
-                self.etat-=1
                 
         if direction=="d": # droite
-            if self.labDuPerso[i][j+1]==0:
-                self.labDuPerso[i][j]=0
-                self.labDuPerso[i][j+1]=3
-                self.position=[i,j+1]
-                
-            elif self.labDuPerso[i][j+1]==2:
-                self.win=True
-                
-            elif self.labDuPerso[i][j+1]==4:
-                self.etat-=1
+            self.direct(i,j+1,i,j)
+                    
                 
         if direction=="z": # haut
-            if self.labDuPerso[i-1][j]==0:
-                self.labDuPerso[i][j]=0
-                self.labDuPerso[i-1][j]=3
-                self.position=[i-1,j]
-                
-            elif self.labDuPerso[i-1][j]==2:
-                self.win=True
-                
-            elif self.labDuPerso[i-1][j]==4:
-                self.etat-=1
-                
-                
+            self.direct(i-1,j,i,j)
+    
+    def monstreTouche(self): #pour que le monstre fasse perdre une vie au perso si il va sur la case où est le personnage
+        for monstre in self.__monstres.values():
+            if monstre.getTouchePerso()==True:
+                self.__etat-=1
+                monstre.reintTouchePerso()
+    
+    
+    def verif_case(self, x, y):
+        """vérifie le type de case destination lors du déplacement"""
+        #à compléter, ou pas
+
+    def inst_relocate(self, new_pos):
+        """si besoin de changer instantanément la position du personnage"""
+        self.__position = new_pos
+    
+    
+    
+    
     def afficherLab(self):
-        taille_sprite = 40
-        L = self.labDuPerso
+        
+        L = self.__labDuPerso
         n = len(L)
         for i in range (n) :
             for j in range (n) :
@@ -101,14 +161,24 @@ class Personnage():
                     screen.blit(arrivee,(x,y))
                 if L[j][i] == 3 :
                     screen.blit(joueur,(x,y))
+                if L[j][i] == 5 :
+                    screen.blit(tp,(x,y))
+                if L[j][i] == 6 :
+                    screen.blit(pot,(x,y))
+                if L[j][i] == 7 :
+                    screen.blit(acc,(x,y))    
+                if L[j][i] == 8 or L[j][i] == 9 :
+                    screen.blit(monstr,(x,y))
+                
                     
                     
                     
         
     
-    def reset(self) : 
-        l=Lab(20)
-        l.initLab(tabMursNiveau1)
-        
-        self.__init__(l,1)
-       
+    def reset(self,niveau) : 
+        l = fich2lab(niveau)
+        self.__init__(l,PV_max)
+        self.__compteur = 0
+        self.initMonstres()
+
+            
---
+++ game.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 21 21:21:15 2021
+
+@author: sabri
+"""
+
+import sys, time, pygame as py
+from Personnage2 import *
+from menu_pause import pause
+from Monstre2 import *
+
+
+py.init()
+
+
+
+#%%
+
+continuer = 1
+
+
+
+while continuer : #boucle principale
+    
+    
+    continuer_jeu = 1
+    continuer_accueil = 1
+    mute = 0  #intéret booleen?
+    
+ #%%   ACCUEIL
+
+    if not mute :
+        py.mixer.music.set_volume(0.4)
+
+    while continuer_accueil :
+        
+        
+        for event in py.event.get() :
+            
+        
+            if event.type == py.QUIT : #fermeture du jeu si l'on appuie sur la croix
+                continuer_accueil = 0
+                continuer_jeu = 0
+                
+                py.display.quit()
+                py.quit()
+                sys.exit()
+                continuer = 0
+                
+            if event.type == py.KEYDOWN : 
+                
+                touche = py.key.name(event.key)
+                # mute
+                if touche == "m" :
+                    if mute :
+                        py.mixer.music.set_volume(0.4)
+                        mute = 0
+                    else :
+                        py.mixer.music.set_volume(0)
+                        mute = 1
+                        
+                if touche == "space" :
+                    continuer_accueil = 0
+                
+                    
+        # animation du logo
+        pamprect = pamprect.move(speed)
+        if pamprect.left < 0 or pamprect.right > width:
+            speed[0] = -speed[0]
+        if pamprect.top < 0 or pamprect.bottom > height:
+            speed[1] = -speed[1]
+
+        screen.fill(black)
+        screen.blit(pamp, pamprect)
+        screen.blit(txt_debut,txt_debutrect)
+        screen.blit(txt_debut2,txt_debutrect2)
+       
+        
+        screen.blit(direc,direc_rect)
+        time.sleep(0.01)
+        py.display.flip() 
+
+     
+       
+    # affichage du niveau 1 et initialisation du jeu
+    lvl = 0
+    l = Lab.fich2lab("niveau_1.txt")
+    p = Personnage(l,PV_max)
+    p.initMonstres()
+    
+    if not mute :
+        py.mixer.music.set_volume(0.2)
+         
+    screen.blit(fond,f_rect)
+    p.afficherLab()
+    py.display.flip()
+    
+#%% JEU
+    
+    
+    while continuer_jeu :
+        
+        p.monstreTouche()
+        for event in py.event.get():
+
+            if event.type == py.QUIT :
+                py.display.quit()
+                py.quit()
+                sys.exit()
+                continuer_jeu = 0
+                continuer = 0
+                
+            if event.type == py.KEYDOWN :
+                
+                touche = py.key.name(event.key)
+                # mute
+                if touche == "m" :
+                    if mute :
+                        py.mixer.music.set_volume(0.2)
+                        mute = 0
+                    else :
+                        py.mixer.music.set_volume(0)
+                        mute = 1
+                  
+                
+                # pause
+                if touche == "escape" :
+                    continuer_jeu,continuer = pause()
+                
+                
+                p.deplacement(touche)
+        
+                
+        if p.get_win() :   #Le joueur est sorti du niveau
+            screen.blit(fond,f_rect)
+            screen.blit(txt_vic,txt_vicrect)
+            py.display.flip() 
+            time.sleep(1)  
+            if lvl < N-1 : #on passe au niveau suivant
+                lvl += 1
+                p.reset(liste_lvl[lvl])
+                
+            elif lvl == N-1 :   #dernier lvl
+                lvl = 0
+                continuer_jeu = 0 #LE JOUEUR A FINI TOUS LES NIVEAUX
+            
+            
+            
+        if p.get_etat() <= 0 :     #Le joueur meurt
+            screen.blit(fond,f_rect)   #changer en screen.fill partout?
+            screen.blit(txt_mort,txt_mortrect)
+            py.display.flip()
+            time.sleep(1)
+            if lvl < N :  #on recommence le niveau
+                p.reset(liste_lvl[lvl])
+            
+        #txt ui      
+        ui = police_ui.render("PV = "+str(p.get_etat()),True,pamplemou,None) 
+        m = police_ui.render("Niveau "+str(lvl)+"       Pause : ECHAP   Mute : M",True,pamplemou,None)
+        ui_rect = ui.get_rect()
+        ui_rect.bottomleft = (5,685)
+        m_rect = m.get_rect()
+        m_rect.bottomright = (640,685)
+        
+        #rafraichit lab
+        screen.fill(fond_c)
+        # screen.blit(fond,f_rect) #changer en screen.fill partout?
+        screen.blit(ui,ui_rect)
+        screen.blit(m,m_rect)
+        
+        p.incrementeCompteur() # pour les monstres
+        p.checkCompteur()
+        
+        
+        p.afficherLab()
+        time.sleep(0.01)
+
+        py.display.flip()
+                    
+        
+    p.reset(liste_lvl[0])
+        
+                    
+
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
